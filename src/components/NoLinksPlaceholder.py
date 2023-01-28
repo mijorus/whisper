107482010136aaa87367412e63edec07256fc1fa
Lorenzo Paderi <lorenzo@mijorus.it>
2023-01-29 00:11:45 +0100

    added placeholder

---
+++ src/components/NoLinksPlaceholder.py
@@ -0,0 +1,18 @@
+from gi.repository import Adw
+from gi.repository import Gtk, GObject
+
+
+class NoLinksPlaceholder(Gtk.Box):
+    def __init__(self, **kwargs):
+        # BTW Github Copilot did this widget for me, without even asking, crazy uh?
+        super().__init__(valign=Gtk.Align.CENTER, orientation=Gtk.Orientation.VERTICAL, spacing=5, vexpand=True)
+
+        title = Gtk.Label(css_classes=['title-1'], label="No active connections")
+        subt = Gtk.Label(css_classes=['dim-label'], label="To get started, connect a microphone and a speaker")
+        icon = Gtk.Image.new_from_icon_name('whisper-info-symbolic')
+        icon.set_css_classes(['dim-label'])
+        icon.set_pixel_size(100)
+
+        self.append(icon)
+        self.append(title)
+        self.append(subt)
